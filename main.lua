6c2ca616e91b78ac9f76d586fa44269a0338c245
MatteoGodzilla <catena.matteo90@gmail.com>
2024-05-06 23:40:54 +0200

    successfully reading midi notes

--- main.lua
+++ main.lua
@@ -22,18 +22,32 @@ local function draw()
     gfx.y = 0
 
     if notesMediaTrack ~= nil then
-        gfx.printf("FOUND NOTES")
+        local midiItem = reaper.GetTrackMediaItem(notesMediaTrack, 0)
+        local midiTake = reaper.GetMediaItemTake(midiItem, 0)
+        if reaper.TakeIsMIDI(midiTake) then
+            local _retval, noteCount, _ccEventCount, _textEventCount = reaper.MIDI_CountEvts(midiTake)
+            for i=0, noteCount - 1 do
+                local _retval, _selected, _muted, startPPQPos, endPPQpos, _channel, pitch, velocity = reaper.MIDI_GetNote(midiTake, i)
+                local startInQB = reaper.MIDI_GetProjQNFromPPQPos(midiTake,startPPQPos)
+                local endInQB = reaper.MIDI_GetProjQNFromPPQPos(midiTake,endPPQpos)
+                gfx.printf("%f \t %f \t %d \t %d", startInQB, endInQB, pitch, velocity)
+                gfx.x = 0
+                gfx.y = gfx.y + gfx.texth
+            end
+        end
+
     else
         gfx.printf("NOTES NOT FOUND")
     end
 
-    gfx.x = 0
-    gfx.y = gfx.texth
-    if effectsMediaTrack ~= nil then
-        gfx.printf("FOUND EFFECTS")
-    else
-        gfx.printf("EFFECTS NOT FOUND")
-    end
+    -- gfx.x = 0
+    -- gfx.y = gfx.y + gfx.texth
+    -- if effectsMediaTrack ~= nil then
+    --     local itemCount = reaper.CountTrackMediaItems(effectsMediaTrack)
+    --     gfx.printf("FOUND EFFECTS: %s", itemCount)
+    -- else
+    --     gfx.printf("EFFECTS NOT FOUND")
+    -- end
 
     gfx.update()
 
@@ -43,7 +57,6 @@ local function draw()
         reaper.defer(draw)
     end
 end
-
 
 local function main()
     init()
